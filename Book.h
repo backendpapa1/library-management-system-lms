cb67907b984a515ab73e2e8116d05a7455f76664
backendpapa1 <akejupaul10@gmail.com>
2023-03-29 03:00:43 +0100

    featured

--- Book.h
+++ Book.h
@@ -6,51 +6,94 @@
 #include <stdio.h>
 #include <fstream>
 
+
 #ifndef LMS_BOOK_H
 #define LMS_BOOK_H
 
 
-class Book {
+using namespace std;
 
+int on_exit(void (*function)(int, void *), void *arg);
+
+class Book {
     nlohmann::json bookRepo;
 
-public:
-    Book(int totals){
-        std::cout << totals << std::endl;
-        std::ofstream file("db.json");
-        std::ifstream f("db.json");
 
+
+public:
+    char bno[6]; // book number
+    char bname[100]; // book name
+    Book(int totals){
+
+
+//        atexit(closeClass(bookRepo));
+
+
+        std::ofstream file("db.json");
         try {
-//            fix data returning
-            nlohmann::json data = nlohmann::json::parse(f);
-            bookRepo = data;
-            std::cout << bookRepo.size() << std::endl;
-        } catch (const nlohmann::json_abi_v3_11_2::detail::parse_error& exc2) {
-            std::cout << "errored" << std::endl;
-            std::cout << exc2.what() << std::endl;
-            bookRepo = R"([])"_json;
-            file << bookRepo;
+            std::ifstream f("../db.json");
+            setlocaldb(f);
+
+
+
+        } catch (const exception& exc2) {
+            cout << "Initializing Database" << endl;
+            initializedb();
         };
 
 
     }
-    void createBook(){
-        char bno[10]; // book number
-        char bname[100]; // book name
 
+    ~Book()
+    {
+        storedata();
+        cout << "closing";
+
+    }
+    void createBook(){
         std::cout << "\nNEW BOOK ENTRY" << std::endl;
         std::cout << "\nENTER BOOK NUMBER" << std::endl;
         std::cin >> bno;
         std::cout << "\nENTER BOOK NAME" << std::endl;
         std::cin >> bname;
-        bookStore(bno,bname);
+        addBook(bno,bname);
+    }
 
+    void showBook(){
+       try{
+           char bcode[6];
+           cout << "\n ENTER BOOK NUMBER";
+           cin >> bcode;
+
+           cout << "checking through db..." << endl;
+           cout << bcode << endl;
+           int bsize = (int) bookRepo.size();
+           bool found = false;
+           int value;
+           for (int i = 0; i < bsize; i++) {
+               if(bookRepo[i]["no"] == bcode){
+                   value = i;
+                   found = true;
+                   break;
+               }
+           };
+               if (!bookRepo[value].is_null() && found) {
+                   cout << "book found! \n";
+                   cout << bookRepo[value] << "\n";
+               } else {
+                   cout << "book not found! \n";
+               }
+
+       } catch (const exception& ex2) {
+           cout << ex2.what() << endl;
+           cout << "book not found! \n";
+       }
     }
 
 
 private:
 
-    void bookStore(char _bno[],char _bname[]){
+    void addBook(char _bno[],char _bname[]){
         std::string bookNo;
         std::string bookName;
         bookNo.append(_bno);
@@ -59,14 +102,34 @@ private:
                 {"no",bookNo},
                 {"name", bookName},
         };
-        std::cout << "working" << bookNo << std::endl;
+        std::cout << "book successfully added" << std::endl;
+        addtodb(ex1);
+        std::cout << bookRepo << bookNo << std::endl;
 
 
     }
 
     void initializedb(){
-
+        nlohmann::json result = nlohmann::json::array();
+        std::ofstream f("../db.json",std::ios_base::trunc |std::ios_base::out);
+        f << result;
     }
+
+    void setlocaldb(ifstream& f){
+        nlohmann::json data = nlohmann::json::parse(f);
+        bookRepo = data;
+    }
+    void addtodb(nlohmann::json& data){
+        bookRepo.push_back(data);
+    }
+
+    void storedata(){
+        std::ofstream closingData("../db.json",std::ios_base::trunc |std::ios_base::out);
+        closingData << bookRepo;
+    }
+
+
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
 #include "Book.h"
+#include "Book.cpp"
+// MAIN LOOP PROBLEMATIC
+void loadMenu(){
+    cout <<"\n\n\tADMINISTRATOR MENU";
+    cout <<"\n\n\t1.BOOK RELATED ACTIVITES";
+    cout <<"\n\n\t-1.EXIT";
+};
 
+void bookMenu(){
+    cout <<"\n\n\tBOOK MENU";
+    cout <<"\n\n\t11.ADD A NEW BOOK";
+    cout <<"\n\n\t12.FIND A BOOK";
+    cout <<"\n\n\t-1.EXIT";
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     Book book(10);
-    book.createBook();
+
+
+    while(true){
+        loadMenu();
+        int key;
+        cin >> key;
+        if(key == 1){
+            bookMenu();
+            cin >> key;
+            if(key == 11){
+                book.createBook();
+            }
+            if(key == 12 ){
+                book.showBook();
+            }
+            key = 0;
+        }
+
+        if(key == -1){
+            cout << "ending application";
+            break;
+        }
+
+    }
     return 0;
 }
